d37ee94972925876389db6466f7117fe682496ac
nilam5922 <nilampatel@Nilams-MacBook-Pro.local>
2022-10-14 23:46:39 -0500

    initial commit

---
+++ add_2_num.py
@@ -0,0 +1,27 @@
+# add to numbers
+from math import remainder
+
+
+# a = 32
+# b = 5
+
+# # print("The sum of a and b is ", a + b)
+
+# # print("The remainder when a is divided by b is ", a % b)
+
+# # print(a < b)
+# # print(a == b)
+
+# a = input("enter first number: ")
+# b = input("enter second number: ")
+# a = int(a)
+# b = int(b)
+# avg = (a + b)/2
+# print("The average if a and b is", avg)
+
+# -------------------------------------------------
+
+a = 40
+b = "Nilam"
+print(type(a))
+print(type(b))
